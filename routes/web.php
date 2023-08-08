d8b5844bb344e12bdc73f27c5b92649bd0585b26
Benson <bensonmakau2000@gmail.com>
2023-08-08 16:19:47 +0300

    dump and die with data helper

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,13 @@ Route::get('/posts/{id}',function($id){
     //The full name for dd() in Laravel is dump and die. 
     //It is a helper function that is used to dump the contents
     // of a variable to the browser and then stop the script execution
-    dd($id); //dump and die
-    // return response('Post '.$id);
+    // dd($id); //dump and die
+
+
+    //the ddd() helper function is a debugging aid that stands for 
+    //"Dump and Die with Data". It's a variation of the dd()
+    // (Dump and Die) function, which is commonly used for debugging 
+    //purposes in PHP and Laravel applications.
+    ddd($id);
+    return response('Post '.$id);
 })->where('id','[0-9]+');
