01d536b7815a3c6e4988da31142416d8704941b2
Benson <bensonmakau2000@gmail.com>
2023-08-08 17:29:56 +0300

    using unless directives

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,11 +1,13 @@
 <h1>{{$heading}}</h1>
 
-@if(count($listings) == 0)
-<p>There are no listings found!</p>
-@endif
+@unless(count($listings) == 0)
  
 @foreach($listings as $listing)
 <h2>{{$listing['title']}}</h2>
 <p>{{$listing['description'];}}</p>
-
 @endforeach
+
+@else
+<p>No Listings Found !</p>
+
+@endunless
