09a9527115e0d7f58bfb373357bf295130767ece
Benson <bensonmakau2000@gmail.com>
2023-08-08 17:07:26 +0300

    deleted basic code used for learning

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', function() {
-    return response()->json([
-        'posts' => [
-            [
-                'title' => 'Post One'
-            ]
-        ]
-    ]);
-});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/hello', function(){
-    return response('<h1>Hello World</h1>',200)
-    ->header('Content-Type', 'text/plain')
-    ->header('foo','bar');
-});
-
-Route::get('/posts/{id}',function($id){
-    //The full name for dd() in Laravel is dump and die. 
-    //It is a helper function that is used to dump the contents
-    // of a variable to the browser and then stop the script execution
-    // dd($id); //dump and die
-
-
-    //the ddd() helper function is a debugging aid that stands for 
-    //"Dump and Die with Data". It's a variation of the dd()
-    // (Dump and Die) function, which is commonly used for debugging 
-    //purposes in PHP and Laravel applications.
-    // ddd($id);
-    return response('Post '.$id);
-})->where('id','[0-9]+');
-
-Route::get('/search', function(Request $request) {
-dd($request->name . ' '. $request->city);
-});
