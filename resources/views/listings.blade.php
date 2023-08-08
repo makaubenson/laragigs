589206f2f8f28d7ca6dfaf753ba9478017cc3c4c
Benson <bensonmakau2000@gmail.com>
2023-08-08 17:27:56 +0300

    using if directives

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,8 +1,9 @@
 <h1>{{$heading}}</h1>
-@php
-$test = 1;
-@endphp
-{{$test}}
+
+@if(count($listings) == 0)
+<p>There are no listings found!</p>
+@endif
+ 
 @foreach($listings as $listing)
 <h2>{{$listing['title']}}</h2>
 <p>{{$listing['description'];}}</p>
