8357e0155c1809989d5959bf610d15414a033144
Benson <bensonmakau2000@gmail.com>
2023-08-08 16:54:31 +0300

    sample api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts', function() {
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post One'
+            ]
+        ]
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
